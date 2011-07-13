3f6d2d1a546ffda49c479d39e68bc5a680130043
Ryan Scott <chiefz.reloaded@gmail.com>
2011-07-13 03:04:48 -0700

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Jul 12 01:07:43 PDT 2011"
+#define LINUX_COMPILE_TIME "01:07:43"
+#define LINUX_COMPILE_BY "chiefzreloaded"
+#define LINUX_COMPILE_HOST "chiefzreloaded-Inspiron-1545"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
